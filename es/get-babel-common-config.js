30a7e264e574b1dbfe5d265c2973a9cc993ddd76
Henry <haovei@qq.com>
2017-12-26 18:15:11 +0800

    ..

--- es/get-babel-common-config.js
+++ es/get-babel-common-config.js
@@ -8,9 +8,10 @@ export default function babel() {
     presets: [presetStage0, [presetEnv, {
       targets: {
         browsers: ['Android >= 4', 'Chrome >= 35', 'Firefox >= 31', 'iOS >= 9', 'Opera >= 12', 'Safari >= 9', 'IE >= 9']
-      }
+      },
+      modules: false
     }]],
-    plugins: [],
+    plugins: [require.resolve('babel-plugin-transform-class-properties'), require.resolve('babel-plugin-transform-decorators-legacy')],
     comments: false
   };
 }
